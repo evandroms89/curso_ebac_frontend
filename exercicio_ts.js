04e5fa9c7cb4aeec013374c3743bfc22460b83d6
Evandro Moraes <evandroms89@gmail.com>
2023-03-16 14:57:13 -0300

    primeiro commit

---
+++ exercicio_ts.js
@@ -0,0 +1,9 @@
+"use strict";
+function multiplicacao(argumento1, argumento2) {
+    return argumento1 * argumento2;
+}
+function saudacao(nome) {
+    return 'Olá ' + nome;
+}
+console.log(multiplicacao(20, 5));
+console.log(saudacao('Maria'));
---
+++ exercicio_ts.ts
@@ -0,0 +1,12 @@
+function multiplicacao(argumento1: number, argumento2: number): number {
+    return argumento1 * argumento2;
+}
+
+function saudacao(nome: string): string {
+    return 'Olá ' + nome;
+}
+
+console.log(multiplicacao(20, 5));
+
+console.log(saudacao('Maria'));
+
